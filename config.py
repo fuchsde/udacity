91f59c426e1a3f1eef426f6dfcd3598f25ae0d1e
fuchsde <32522107+fuchsde@users.noreply.github.com>
2020-12-28 13:59:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'udacityblob123'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'dSf3pPgJ96s++7cM84lTTNSH8Kx4s0uGiTF6ZToGy7OWWKdfBelHnyV0dWFOq2QjDxVwnhQV80VQs7HTbmkPpQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'udactiy-sql-server.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'udacity-project-sql'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacity-dennis'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Dec2812!'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
